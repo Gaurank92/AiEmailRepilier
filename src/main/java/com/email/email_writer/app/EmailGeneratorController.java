4d8019e8e2bf806a4737b8f799e5f84248322807
Gaurank <gaurank92@gmail.com>
2025-07-20 07:55:42 +0530

    allowed corssed origins in controller

--- src/main/java/com/email/email_writer/app/EmailGeneratorController.java
+++ src/main/java/com/email/email_writer/app/EmailGeneratorController.java
@@ -3,14 +3,12 @@ package com.email.email_writer.app;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")   // ******* FRONT-END requests allows and from  any other place.....
 public class EmailGeneratorController {
 
     private  final EmailGeneratorService emailGeneratorService;
